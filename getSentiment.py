64faebb0179bee1f2f56650d8d4fe98a66a8f318
Juan Vargas-Murillo <juanvmurillo@gmail.com>
2019-03-01 16:20:38 -0700

    simple get endpoint for dummy data

--- getSentiment.py
+++
@@ -1,9 +0,0 @@
-class GetSentiment():
-    def getSentiment(self,obj) :
-        #to do
-        print("From getSentiment")
-        print(obj["location"])
-        sentScore = ['happy','angry','sad','happy','happy']
-        retRes = [{'location': obj['location'], 'timestamp': obj['timestamp'],'hashtags': obj['hashtags'],'sentiment':sentScore}]
-        return retRes
-        
---
+++ rest-twitter/static/js/home.js
@@ -0,0 +1,34 @@
+/*
+* Javascript file for the applcation demonstration
+* using the API
+*/
+
+// Create the namespace instance
+let ns = {};
+
+// Create the model instance
+ns.model = (function() {
+    'use strict';
+
+    let $event_pump = $('body');
+
+    // Return the API
+    return {
+        'read': function() {
+            let ajax_options = {
+                type: 'GET',
+                url: 'api/tweets',
+                accepts: 'application/json',
+                dataType: 'json'
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_read_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown){
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        }
+    };
+}());
+
