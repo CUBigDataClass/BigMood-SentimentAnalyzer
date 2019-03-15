b45e5f1193bda71f6bc155d73b49a65d0f7cdc32
srishtirawal <srishti.rawal2012@vit.ac.in>
2019-03-15 16:36:51 -0600

    integrate fetch tweets with vader sentiment

--- rest-twitter/service/TweetRetriever_Test.py
+++ rest-twitter/service/TweetRetriever_Test.py
@@ -3,9 +3,23 @@
 # Make sure to run with e.g. "env TWITTER_API_KEY=xxx TWITTER_API_KEY_SECRET=xxx python TweetRetriever_Test.py"
 
 from TweetRetriever import TweetRetriever
+from vaderSent import SentimentAnalyzer
+import time
+from config.location_service_config import CACHE
+from service.Location_Service import *
+import os
 
 tr = TweetRetriever()
+vs = SentimentAnalyzer()
+path = os.path.join(os.path.curdir, CACHE['location'])
+ls = LocationService(path, CACHE['dump_interval'])
 
-tweets = tr.get_tweets('cold','39.7392358','-104.990251')
+coords = ls.get_coordinates_for_city({'city': 'denver', 'country': 'US'})
+tweets = tr.get_tweets('#ClimateStrike','39.7392358','-104.990251')
 
-print(tweets)
+for each in tweets:
+    score = vs.sentimentAnalyzerScores(each)
+    print(each, score)
+    print("#####")
+avgSent = vs.computeSentiment(tweets)
+print(avgSent)
