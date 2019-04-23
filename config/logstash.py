11cd4a8f0715c6cbeb3c24fa1f850c2d546544c8
Juan Vargas-Murillo <juanvmurillo@gmail.com>
2019-04-22 23:32:42 -0600

    .gitignore fix

--- config/logstash.py
+++
@@ -1,2 +0,0 @@
-logstash_host = "34.66.188.155"
-logstash_port = 9123
--- config/twitter_keys.py
+++
@@ -1,2 +0,0 @@
-consumer_key = "hb4S0J73dRVHMDrAPrXGeY6TH"
-consumer_secret = "xWUkV0g9b64asJfp0AnSYUuDWvANLpKxctsySB8wGx3Fo7jdUP"
